8e1bb42f36cb4a22a77973dfc41be736cd96aaa3
Ximena Zuberbuhler <37660743+ximezuber@users.noreply.github.com>
2021-10-28 11:46:22 -0300

    Hough Transformation

--- src/utils/borders_utils.py
+++ src/utils/borders_utils.py
@@ -1,9 +1,10 @@
 import math
 
-from PIL import Image
+from PIL import Image, ImageDraw
 import numpy as np
 
 from numpy import copy, int32, uint8
+from numpy.core.fromnumeric import nonzero
 
 from src.utils.image_utils import synthesis, normalize
 from src.utils.mask_utils import fill_outer_matrix, sobel_vertical_mask, sobel_horizontal_mask
@@ -366,4 +367,134 @@ def has_finished_active_contours(img, avg_obj_color, epsilon, lin, lout):
         if fd(img[pixel[0]][pixel[1]], avg_obj_color, epsilon) > 0:
             return False
     return True
+
+
+def hough_linear(img, epsilon, t1, t2, theta_step: int = 7, rho_step: int = 5):
+    border_img = canny(img, t1, t2)
+    width = img.shape[0]
+    height = img.shape[1]
+    d = np.max([width, height])
+    rho = math.sqrt(2) * d
+
+    rho_range = np.arange(-rho, rho, rho_step)
+    theta_range = np.deg2rad(np.arange(-90, 90, theta_step))
+    theta_cos = np.cos(theta_range)
+    theta_sin = np.sin(theta_range)
+
+    edges = np.asarray(border_img == 255).nonzero()
+    accumulator = np.zeros((len(theta_range), len(rho_range)))
+    
+    coordinates = list(zip(edges[0], edges[1]))
+
+
+    for p in range(len(coordinates)):
+        for theta_idx in range(len(theta_range)):
+            for rho_idx in range(len(rho_range)):
+                # Veo si cumple la ecuacion de la recta
+                if (
+                    abs(
+                        rho_range[rho_idx]
+                        - coordinates[p][1] * theta_cos[theta_idx]
+                        - coordinates[p][0] * theta_sin[theta_idx]
+                    )
+                    < epsilon
+                ):
+                    accumulator[theta_idx, rho_idx] += 1
+
+    result = np.asarray(Image.fromarray(img).convert(mode='RGB'))
+    max = np.max(accumulator)
+    for rho_idx in range(len(rho_range)):
+        for theta_idx in range(len(theta_range)):
+            if accumulator[theta_idx, rho_idx] >= 0.8 * max:
+                result = draw_lines(result, rho_range[rho_idx], theta_range[theta_idx], epsilon)
+    return result
+
+
+def draw_lines(img, rho, theta, epsilon):
+
+    if(theta == 0):
+        x1 = rho * np.cos(theta)
+        x2 = x1
+        y1 = 0 
+        y2 = img.shape[1]
+    else:
+        x0 = rho * np.cos(theta)
+        y0 = rho * np.sin(theta)
+        m = - 1 / np.tan(theta)
+        xs = []
+        ys = []
+        for x in [0, img.shape[0]]:
+            if(linar_func(x0, y0, m, x) <= img.shape[1] and linar_func(x0, y0, m, x) >= 0):
+                xs.append(x)
+
+        for y in [0, img.shape[1]]:
+            if(linar_func_x(x0, y0, m, y) <= img.shape[0] and linar_func_x(x0, y0, m, y) >= 0):
+                ys.append(y)
+        
+        if len(xs) == 1 and len(ys) == 1:
+            x1 = xs[0]
+            y1 = int(linar_func(x0, y0, m, x1))
+            y2 = ys[0]
+            x2 = int(linar_func_x(x0, y0, m, y2))
+        elif(len(xs) == 2):
+            x1 = xs[0]
+            x2 = xs[1]
+            y1 = int(linar_func(x0, y0, m, x1))
+            y2 = int(linar_func(x0, y0, m, x2))
+        elif(len(ys) == 2):  
+            y1 = ys[0]
+            y2 = ys[1]
+            x1 = int(linar_func_x(x0, y0, m, y1))
+            x2 = int(linar_func_x(x0, y0, m, y2))    
+        
+    aux_img = Image.fromarray(img)
+    draw_img = ImageDraw.Draw(aux_img)
+    draw_img.line([(x1, y1), (x2, y2)], fill='red')
+    return np.asarray(aux_img)
+
+    # a = np.cos(theta)
+    # b = np.sin(theta)
+
+    # for x in range(0, img.shape[0]):
+    #     for y in range(0, img.shape[1]):
+    #         if abs(rho - x * a - y * b) < epsilon:
+    #             img[x, y, 0] = 255
+    #             img[x, y, 1] = 0
+    #             img[x, y, 2] = 0
+
+    # return img
+    # a = np.cos(theta)
+    # b = np.sin(theta)
+    # x0 = a * rho
+    # y0 = b * rho
+
+    # x1 = int(x0 + 1000 * (-b))
+    # y1 = int(y0 + 1000 * (a))
+    # x2 = int(x0 - 1000 * (-b))
+    # y2 = int(y0 - 1000 * (a))
+    # if x1 == x2:
+    #     for y in range(0, img.shape[0]):
+    #         img[y, int(x0), 0] = 255
+    #         img[y, int(x0), 1] = 0
+    #         img[y, int(x0), 2] = 0
+
+    # else:
+    #     slope = (y2 - y1) / (x2 - x1)
+    #     origin_ordenate = y0 - slope * x0
+    #     for x in range(0, img.shape[0]):
+    #         y = int(slope * x + origin_ordenate)
+    #         # y = int(- ((np.cos(theta) / np.sin(theta)) * x) + rho / np.sin(theta))
+    #         if 0 <= y < img.shape[1]:
+    #             img[y, x, 0] = 255
+    #             img[y, x, 1] = 0
+    #             img[y, x, 2] = 0
+    # return img
+
+
+def linar_func(x0, y0, m, x):
+    return m * (x - x0) + y0
+
+
+def linar_func_x(x0, y0, m, y):
+    return (y - y0 + m * x0) / m
 
